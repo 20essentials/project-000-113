74b0e65becca2534a8ff79c828a3357e9a23b749
20essentials <s0l0estudi4r@gmail.com>
2024-10-04 12:06:31 +0200

    v0.0.1

---
+++ script.js
@@ -0,0 +1,11 @@
+console.groupCollapsed('Reference');
+console.log(
+  'Scroll Image From Top To Bottom',
+  'https://youtu.be/mCwkbiEywFY?si=nco5gYp4MZzABOc-'
+);
+console.log(
+  'Extension: Full Page Screen Capture',
+  'https://chromewebstore.google.com/detail/gofullpage-full-page-scre/fdpohaocaechififmbbbbbknoalclacl'
+);
+console.log('Wallhaven', 'https://wallhaven.cc/toplist');
+console.groupEnd();
